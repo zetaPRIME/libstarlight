7e600224e60ec249793ac5084d6b747e1b6f80e6
zetaPRIME <qmanxt@gmail.com>
2017-03-03 06:59:07 -0500

    welp, that broke the forward declaration and I can't actually fix it

--- libstarlight/source/starlight/_incLib/json_fwd.hpp
+++ libstarlight/source/starlight/_incLib/json_fwd.hpp
@@ -1,31 +1,3 @@
+// okay, 2.1.x broke the forward declaration so here's a redirect until it gets proper fwd
 #pragma once
-
-#ifndef NLOHMANN_JSON_FWD_HPP
-#define NLOHMANN_JSON_FWD_HPP
-
-#include <cstdint>
-#include <map>
-#include <vector>
-#include <string>
-
-namespace nlohmann
-{
-
-template <
-    template<typename U, typename V, typename... Args> class ObjectType,
-    template<typename U, typename... Args> class ArrayType,
-    class StringType,
-    class BooleanType,
-    class NumberIntegerType,
-    class NumberUnsignedType,
-    class NumberFloatType,
-    template<typename U> class AllocatorType
-    >
-class basic_json;
-
-using json = basic_json<std::map,std::vector,std::string,bool,std::int64_t,
-                        std::uint64_t,double,std::allocator>;
-
-} // namespace nlohmann
-
-#endif // NLOHMANN_JSON_FWD_HPP
+#include "json.hpp"
